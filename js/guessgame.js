bdd16f5fa625d80a36f0c351ff0ed62f8f4b5399
Renato Martins <renato.martins@latitudde.com>
2023-07-28 01:53:40 +0100

    Implement guess number score

--- js/guessgame.js
+++ js/guessgame.js
@@ -1,12 +1,14 @@
-'use strict'
+"use strict";
 
 let startButton = document.querySelector("#startBtn");
 let guessButton = document.querySelector("#checkNumberBtn");
 let messageField = document.querySelector("#message");
 let userGuessField = document.querySelector("#userGuessNumber");
-let container=document.querySelector(".container");
-guessButton.disabled;
+let container = document.querySelector(".container");
+let currentScore=document.querySelector("#currentScore");
+guessButton.disabled = true;
 let randomNumber = 0;
+let highScore=10;
 
 /* The code block `startButton.addEventListener("click", function () { ... })` is an event listener
 that listens for a click event on the `startButton` element. When the button is clicked, the code
@@ -14,9 +16,12 @@ inside the event listener is executed. In this case, it generates a random numbe
 using `Math.random()` and `Math.floor()`, and assigns it to the `randomNumber` variable. It also
 logs the generated number to the console. */
 startButton.addEventListener("click", function () {
-  randomNumber = Math.floor((Math.random() * 10));
+  randomNumber = Math.trunc(Math.random() * 10 + 1);
   console.log("start game " + randomNumber);
   container.style["background-color"] = "#f0f0f0";
+  guessButton.disabled = false;
+  userGuessField.value = "";
+  currentScore.textContent=10;
 });
 
 /* The code block inside the `guessButton.addEventListener("click", () => { ... })` is an event
@@ -27,12 +32,15 @@ guessButton.addEventListener("click", () => {
   console.log("Given number " + givenNumber);
 
   if (givenNumber > randomNumber) {
-    messageField.textContent="Too high";
+    messageField.textContent = "Too high";
+    highScore-=1;
+    currentScore.textContent=highScore;
   } else if (givenNumber < randomNumber) {
-    messageField.textContent="Too low";
+    messageField.textContent = "Too low";
+    highScore-=1;
+    currentScore.textContent=highScore;
   } else {
-    console.log(container);
-    messageField.textContent="You won";
+    messageField.textContent = "You won";
     container.style["background-color"] = "#0F0";
   }
 });
