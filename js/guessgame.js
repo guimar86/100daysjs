ddc4683aae2ca924d97a1fa8e2fb63e8d0d37c36
Renato Martins <renato.martins@latitudde.com>
2023-07-29 18:22:54 +0100

    Text animations added

--- js/guessgame.js
+++ js/guessgame.js
@@ -5,10 +5,10 @@ let guessButton = document.querySelector("#checkNumberBtn");
 let messageField = document.querySelector("#message");
 let userGuessField = document.querySelector("#userGuessNumber");
 let container = document.querySelector(".container");
-let currentScore=document.querySelector("#currentScore");
+let currentScore = document.querySelector("#currentScore");
 guessButton.disabled = true;
 let randomNumber = 0;
-let highScore=10;
+let highScore = 10;
 
 /* The code block `startButton.addEventListener("click", function () { ... })` is an event listener
 that listens for a click event on the `startButton` element. When the button is clicked, the code
@@ -21,26 +21,46 @@ startButton.addEventListener("click", function () {
   container.style["background-color"] = "#f0f0f0";
   guessButton.disabled = false;
   userGuessField.value = "";
-  currentScore.textContent=10;
+  currentScore.textContent = 10;
+  highScore = 10;
 });
 
 /* The code block inside the `guessButton.addEventListener("click", () => { ... })` is an event
 listener that listens for a click event on the `guessButton` element. When the button is clicked,
 the code inside the event listener is executed. */
 guessButton.addEventListener("click", () => {
-  let givenNumber = userGuessField.value;
+  let givenNumber = Number(userGuessField.value);
   console.log("Given number " + givenNumber);
 
-  if (givenNumber > randomNumber) {
+  if (givenNumber === 0) {
+    messageField.classList.add("animate__animated", "animate__shakeX");
+    messageField.textContent = "Please insert a valid number";
+    console.log(messageField);
+    messageField.addEventListener("animationend", () => {
+      messageField.classList.remove("animate__animated", "animate__shakeX");
+    });
+    return;
+  } else if (givenNumber > randomNumber) {
     messageField.textContent = "Too high";
-    highScore-=1;
-    currentScore.textContent=highScore;
+    highScore -= 1;
+    currentScore.textContent = highScore;
+    lostGame(highScore);
   } else if (givenNumber < randomNumber) {
     messageField.textContent = "Too low";
-    highScore-=1;
-    currentScore.textContent=highScore;
+    highScore -= 1;
+    currentScore.textContent = highScore;
+    lostGame(highScore);
   } else {
+    console.log(container);
     messageField.textContent = "You won";
     container.style["background-color"] = "#0F0";
   }
 });
+
+function lostGame(highScore) {
+  if (highScore == 0) {
+    container.style["background-color"] = "#F00";
+    messageField.textContent = "You lost";
+    guessButton.disabled = true;
+  }
+}
