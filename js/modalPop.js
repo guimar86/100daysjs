21e69c09cdfc6eecd2f7c04228e933795f785eb1
Renato Martins <renato.martins@latitudde.com>
2023-08-06 19:51:40 +0100

    Modal fixes

--- js/modalPop.js
+++ js/modalPop.js
@@ -1,4 +1,30 @@
 "use strict";
 
-let modalDiv=document.querySelector(".modal");
-modalDiv.style.display="none";
+let modal=document.querySelector(".modal");
+let overlay=document.querySelector(".overlay");
+let closeModalBtn=document.querySelector("#modalCloseBtn");
+let openModalBtn=document.querySelector("#openModal");
+
+
+const openModal=function(){
+
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+
+};
+
+const closeModal=function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+closeModalBtn.addEventListener("click",closeModal);
+openModalBtn.addEventListener("click",openModal);
+overlay.addEventListener("click",closeModal);
+document.addEventListener("keyup",function(e){
+
+    if(e.key==="Escape" && !modal.classList.contains("hidden")){
+
+        closeModal();
+    }
+})
